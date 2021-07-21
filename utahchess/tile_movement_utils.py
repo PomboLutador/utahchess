42e7da5cda64734f99b29c4583441c4642ef6c16
Tizian Bluntschli <tizian.bluntschli@gmail.com>
2021-07-21 21:15:24 +0100

    Add function to apply movement vector n times

--- utahchess/tile_movement_utils.py
+++ utahchess/tile_movement_utils.py
@@ -7,5 +7,20 @@ def apply_movement_vector(
     return position[0] + movement_vector[0], position[1] + movement_vector[1]
 
 
+def apply_movement_vector_n_times(
+    position: tuple[int, int],
+    movement_vector: tuple[int, int],
+    n: int,
+) -> tuple[int, int]:
+    position_to_return = position
+    while n > 0:
+        position_to_return = (
+            position_to_return[0] + movement_vector[0],
+            position_to_return[1] + movement_vector[1],
+        )
+        n -= 1
+    return position_to_return
+
+
 def is_in_bounds(position: tuple[int, int]) -> bool:
     return (0 <= position[0] <= 7) & (0 <= position[1] <= 7)
