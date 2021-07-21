14d67d7bd4e17fcdaf431d5375b0bcbcb45159e0
Tizian Bluntschli <noemail>
2021-07-21 21:36:25 +0100

    Run formatter and parametrize castling tests

--- tests/test_castling.py
+++ tests/test_castling.py
@@ -1,107 +1,102 @@
+import pytest
+
 from utahchess.board import Board
 from utahchess.castling import CastlingMove, get_castling_moves
 
 
-def test_castling_top_left():
-    # when
-    board_string = f"""br-oo-oo-oo-bc-bb-bk-br
+@pytest.mark.parametrize(
+    (
+        "board_string",
+        "rook_from_position",
+        "rook_to_position",
+        "king_from_position",
+        "king_to_position",
+        "current_player",
+    ),
+    [
+        (
+            f"""br-oo-oo-oo-bc-bb-bk-br
             bp-bp-bp-bp-bq-bp-bp-bp
             oo-oo-oo-oo-bp-oo-oo-oo
             oo-oo-oo-oo-oo-oo-oo-oo
             oo-oo-oo-oo-oo-oo-wp-oo
             oo-oo-oo-oo-oo-wp-oo-oo
             wp-wp-wp-wp-wp-oo-oo-wp
-            wr-wk-wb-wq-wc-wb-wk-wr"""
-    board = Board(board_string=board_string)
-    expected_piece_moves = (
-        ((0, 0), (3, 0)),
-        ((4, 0), (2, 0)),
-    )
-    expected = (
-        CastlingMove(
-            piece_moves=expected_piece_moves, moving_pieces=(board[0, 0], board[4, 0])
+            wr-wk-wb-wq-wc-wb-wk-wr""",
+            (0, 0),
+            (3, 0),
+            (4, 0),
+            (2, 0),
+            "black",
         ),
-    )
-
-    # then
-    actual = tuple(get_castling_moves(board=board, current_player="black"))
-    assert expected == actual
-
-
-def test_castling_top_right():
-    # when
-    board_string = f"""br-bk-bb-oo-bc-oo-oo-br
+        (
+            f"""br-bk-bb-oo-bc-oo-oo-br
             bp-bp-bp-bp-bq-bp-bp-bp
             oo-oo-oo-oo-bp-oo-oo-oo
             oo-oo-oo-oo-oo-oo-oo-oo
             oo-oo-oo-oo-oo-oo-wp-oo
             oo-oo-oo-oo-oo-wp-oo-oo
             wp-wp-wp-wp-wp-oo-oo-wp
-            wr-wk-wb-wq-wc-wb-wk-wr"""
-    board = Board(board_string=board_string)
-    expected_piece_moves = (
-        ((7, 0), (5, 0)),
-        ((4, 0), (6, 0)),
-    )
-    expected = (
-        CastlingMove(
-            piece_moves=expected_piece_moves, moving_pieces=(board[7, 0], board[4, 0])
+            wr-wk-wb-wq-wc-wb-wk-wr""",
+            (7, 0),
+            (5, 0),
+            (4, 0),
+            (6, 0),
+            "black",
         ),
-    )
-
-    # then
-    actual = tuple(get_castling_moves(board=board, current_player="black"))
-    assert expected == actual
-
-
-def test_castling_bottom_left():
-    # when
-    board_string = f"""br-bk-bb-oo-bc-bb-bk-br
+        (
+            f"""br-bk-bb-oo-bc-bb-bk-br
             bp-bp-bp-bp-bq-bp-bp-bp
             oo-oo-oo-oo-bp-oo-oo-oo
             oo-oo-oo-oo-oo-oo-oo-oo
             oo-oo-oo-oo-oo-oo-wp-oo
             oo-oo-oo-oo-oo-wp-oo-oo
             wp-wp-wp-wp-wp-oo-oo-wp
-            wr-oo-oo-oo-wc-wb-wk-wr"""
-    board = Board(board_string=board_string)
-    expected_piece_moves = (
-        ((0, 7), (3, 7)),
-        ((4, 7), (2, 7)),
-    )
-    expected = (
-        CastlingMove(
-            piece_moves=expected_piece_moves, moving_pieces=(board[0, 7], board[4, 7])
+            wr-oo-oo-oo-wc-wb-wk-wr""",
+            (0, 7),
+            (3, 7),
+            (4, 7),
+            (2, 7),
+            "white",
         ),
-    )
-
-    # then
-    actual = tuple(get_castling_moves(board=board, current_player=("white")))
-    assert expected == actual
-
-
-def test_castling_bottom_right():
-    # when
-    board_string = f"""br-bk-bb-oo-bc-bb-bk-br
+        (
+            f"""br-bk-bb-oo-bc-bb-bk-br
             bp-bp-bp-bp-bq-bp-bp-bp
             oo-oo-oo-oo-bp-oo-oo-oo
             oo-oo-oo-oo-oo-oo-oo-oo
             oo-oo-oo-oo-oo-oo-wp-oo
             oo-oo-oo-oo-oo-wp-oo-oo
             wp-wp-wp-wp-wp-oo-oo-wp
-            wr-wk-wb-wq-wc-oo-oo-wr"""
+            wr-wk-wb-wq-wc-oo-oo-wr""",
+            (7, 7),
+            (5, 7),
+            (4, 7),
+            (6, 7),
+            "white",
+        ),
+    ],
+)
+def test_castling_in_corners(
+    board_string,
+    rook_from_position,
+    rook_to_position,
+    king_from_position,
+    king_to_position,
+    current_player,
+):
+    # when
     board = Board(board_string=board_string)
-
     expected_piece_moves = (
-        ((7, 7), (5, 7)),
-        ((4, 7), (6, 7)),
+        (rook_from_position, rook_to_position),
+        (king_from_position, king_to_position),
     )
     expected = (
         CastlingMove(
-            piece_moves=expected_piece_moves, moving_pieces=(board[7, 7], board[4, 7])
+            piece_moves=expected_piece_moves,
+            moving_pieces=(board[rook_from_position], board[king_from_position]),
         ),
     )
 
     # then
-    actual = tuple(get_castling_moves(board=board, current_player=("white")))
+    actual = tuple(get_castling_moves(board=board, current_player=current_player))
     assert expected == actual
