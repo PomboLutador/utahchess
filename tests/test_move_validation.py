e57357d157540a2908cc185ce0a7a19044e6f1b6
Tizian Bluntschli <tizian.bluntschli@gmail.com>
2021-07-21 08:02:58 +0100

    Add check and checkmate functions

---
+++ tests/test_move_validation.py
@@ -0,0 +1,66 @@
+from utahchess.board import Board
+from utahchess.move_validation import is_check, is_checkmate
+
+
+def test_fools_mate():
+    # when
+    board_string = f"""br-bk-bb-oo-bc-bb-bk-br
+            bp-bp-bp-bp-oo-bp-bp-bp
+            oo-oo-oo-oo-bp-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-wp-bq
+            oo-oo-oo-oo-oo-wp-oo-oo
+            wp-wp-wp-wp-wp-oo-oo-wp
+            wr-wk-wb-wq-wc-wb-wk-wr"""
+    board = Board(board_string=board_string)
+    print(board)
+    # then
+    assert is_checkmate(board=board, current_player="white")
+
+
+def test_friendly_piece_can_save_king():
+    # when
+    board_string = f"""br-oo-wq-bq-bc-bb-oo-br
+            bp-bp-oo-oo-oo-oo-oo-oo
+            bb-oo-oo-oo-oo-bp-oo-oo
+            wp-oo-wb-oo-oo-oo-oo-wb
+            oo-oo-oo-oo-oo-oo-oo-oo
+            wk-oo-oo-oo-oo-oo-oo-oo
+            oo-wp-wc-oo-oo-oo-wp-oo
+            oo-wr-oo-bk-oo-oo-wk-wr"""
+    board = Board(board_string=board_string)
+
+    # then
+    assert not is_checkmate(board=board, current_player="black")
+
+
+def test_friendly_piece_can_save_king_two():
+    # when
+    board_string = f"""br-oo-oo-oo-oo-bb-oo-br
+            bp-bp-oo-bb-oo-oo-bp-bp
+            bk-oo-oo-bp-oo-bc-oo-oo
+            oo-wb-oo-oo-bp-oo-oo-oo
+            oo-oo-wp-oo-wp-wp-bk-oo
+            wp-oo-wp-oo-oo-oo-oo-oo
+            oo-wb-oo-oo-oo-bq-wp-wp
+            wr-wk-oo-oo-oo-wq-wc-wr"""
+    board = Board(board_string=board_string)
+
+    # then
+    assert not is_checkmate(board=board, current_player="white")
+
+
+def test_another_scenario():
+    # when
+    board_string = f"""br-oo-oo-wq-oo-bc-oo-br
+            oo-oo-oo-oo-bk-oo-bp-oo
+            oo-wb-oo-oo-wp-bp-oo-oo
+            oo-bp-oo-oo-oo-wp-wk-oo
+            bq-wp-oo-oo-wp-oo-oo-bp
+            bb-oo-wp-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-wk-oo-wb-oo-wk-oo-wr"""
+    board = Board(board_string=board_string)
+
+    # then
+    assert not is_checkmate(board=board, current_player="black")
--- utahchess/move_validation.py
+++ utahchess/move_validation.py
@@ -4,6 +4,7 @@ from typing import Generator
 
 from utahchess.board import Board
 from utahchess.move import Move
+from utahchess.move_candidates import get_all_move_candidates
 from utahchess.piece import Piece
 
 
@@ -25,12 +26,51 @@ class RegularMove(Move):
         pass
 
 
-def is_check(board: Board) -> bool:
-    pass
+def is_check(board: Board, current_player: str) -> bool:
+    """Checks if current_player is in check.
+
+    En Passant and Castling moves do not have to be considered
+    as those are incapable of eating the current player's king.
+    """
+    enemy_color = "white" if current_player == "black" else "black"
+    current_player_king_position = _find_current_players_king_position(
+        board=board, current_player=current_player
+    )
+    all_possible_enemy_move_candidates = get_all_move_candidates(
+        board=board, current_player=enemy_color
+    )
+
+    all_possible_enemy_destinations = [
+        move[1] for move in all_possible_enemy_move_candidates
+    ]
+
+    return current_player_king_position in all_possible_enemy_destinations
 
 
-def is_checkmate(board: Board) -> bool:
-    pass
+def _find_current_players_king_position(
+    board: Board, current_player: str
+) -> tuple[int, int]:
+    for piece in board.all_pieces():
+        if piece.piece_type == "King" and piece.color == current_player:
+            return piece.position
+    raise Exception(f"No King found for {current_player}")
+
+
+def is_checkmate(board: Board, current_player: str) -> bool:
+    """Check if current player is in checkmate."""
+
+    def all_possible_boards():
+        all_move_candidates = get_all_move_candidates(
+            board=board, current_player=current_player
+        )
+        for move_candidate in all_move_candidates:
+            from_position, to_position = move_candidate
+            yield board.move_piece(from_position=from_position, to_position=to_position)
+
+    for possible_board in all_possible_boards():
+        if not is_check(board=possible_board, current_player=current_player):
+            return False
+    return is_check(board=board, current_player=current_player)
 
 
 def validate_move_candidates(
