b17d0955d2526392ed1c349fecd4c13643d623eb
Tizian Bluntschli <tizian.bluntschli@gmail.com>
2021-07-21 19:50:14 +0100

    Move function to ABC, add is_capturing_move

--- utahchess/move.py
+++ utahchess/move.py
@@ -9,3 +9,7 @@ class Move(abc.ABC):
     move_type: str
     piece_moves: tuple[tuple[tuple[int, int], tuple[int, int]], ...]
     moving_pieces: tuple[Piece, ...]
+    is_capturing_move: bool
+
+    def get_algebraic_notation(self) -> str:
+        pass
