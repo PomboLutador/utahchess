5690d6082b74e83945814287c57b02f91f6ac747
Tizian Bluntschli <tizian.bluntschli@gmail.com>
2021-07-24 16:42:57 +0100

    Validate en passant moves and add more tests

--- utahchess/en_passant.py
+++ utahchess/en_passant.py
@@ -5,7 +5,7 @@ from typing import Generator, Optional
 
 from utahchess.board import Board, is_occupied
 from utahchess.move import Move
-from utahchess.move_validation import RegularMove
+from utahchess.move_validation import is_check
 from utahchess.piece import Piece
 from utahchess.tile_movement_utils import apply_movement_vector, is_in_bounds
 
@@ -63,18 +63,34 @@ def get_en_passant_moves(
             board[initial_tile].piece_type == "Pawn"
             and board[last_move.piece_moves[0][1]].color != board[initial_tile].color
         ):
-            yield EnPassantMove(
+            potential_move = EnPassantMove(
                 piece_moves=((initial_tile, destination_tile),),
                 moving_pieces=(board[initial_tile],),
             )
+            if _is_valid_en_passant_move(board=board, en_passant_move=potential_move):
+                yield potential_move
 
 
-def complete_en_passant_move(board: Board, en_passant_move: EnPassantMove) -> Board:
+def _complete_en_passant_move(board: Board, en_passant_move: EnPassantMove) -> Board:
     """Completes the capturing part of an en passant move."""
-    pass
+    piece_move = en_passant_move.piece_moves[0]
+    movement_direction = piece_move[1][1] - piece_move[0][1]
+    tile_to_delete = apply_movement_vector(
+        position=piece_move[1], movement_vector=(0, -movement_direction)
+    )
+    return board.delete_piece(position=tile_to_delete)
 
 
-def _get_piece_move_for_en_passant(
-    board: Board, last_move: Optional[Move]
-) -> tuple[tuple[int, int], tuple[int, int]]:
-    pass
+def make_en_passant_move(board: Board, en_passant_move: EnPassantMove) -> Board:
+    piece_move = en_passant_move.piece_moves[0]
+    board = board.move_piece(from_position=piece_move[0], to_position=piece_move[1])
+    board = _complete_en_passant_move(board=board, en_passant_move=en_passant_move)
+    return board
+
+
+def _is_valid_en_passant_move(board: Board, en_passant_move: EnPassantMove) -> bool:
+    current_player = en_passant_move.moving_pieces[0].color
+    board_after_move = make_en_passant_move(
+        board=board, en_passant_move=en_passant_move
+    )
+    return not is_check(board=board_after_move, current_player=current_player)
