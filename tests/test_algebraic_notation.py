d66144c19abcb6e64a3cbdfdcd1b1728c62b5a87
Tizian Bluntschli <noemail>
2021-07-25 11:59:39 +0100

    Add functions to convert rank and file to indices

--- tests/test_algebraic_notation.py
+++ tests/test_algebraic_notation.py
@@ -1,6 +1,11 @@
 import pytest
 
-from utahchess.algebraic_notation import x_index_to_file, y_index_to_rank
+from utahchess.algebraic_notation import (
+    file_to_x_index,
+    rank_to_y_index,
+    x_index_to_file,
+    y_index_to_rank,
+)
 
 
 @pytest.mark.parametrize(
@@ -43,3 +48,45 @@ def test_y_index_to_rank(y_index, expected_rank):
 
     # then
     assert result == expected_rank
+
+
+@pytest.mark.parametrize(
+    ("rank", "expected_y_index"),
+    [
+        ("8", 0),
+        ("7", 1),
+        ("6", 2),
+        ("5", 3),
+        ("4", 4),
+        ("3", 5),
+        ("2", 6),
+        ("1", 7),
+    ],
+)
+def test_rank_to_y_index(rank, expected_y_index):
+    # when
+    result = rank_to_y_index(rank=rank)
+
+    # then
+    assert result == expected_y_index
+
+
+@pytest.mark.parametrize(
+    ("file", "expected_x_index"),
+    [
+        ("a", 0),
+        ("b", 1),
+        ("c", 2),
+        ("d", 3),
+        ("e", 4),
+        ("f", 5),
+        ("g", 6),
+        ("h", 7),
+    ],
+)
+def test_file_to_x_index(file, expected_x_index):
+    # when
+    result = file_to_x_index(file=file)
+
+    # then
+    assert result == expected_x_index
