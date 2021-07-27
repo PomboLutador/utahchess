3a38a30474f11c95394874d270cd535fed8bd228
Tizian Bluntschli <tizian.bluntschli@gmail.com>
2021-07-27 21:54:23 +0100

    Move important function to the top of module

--- utahchess/legal_moves.py
+++ utahchess/legal_moves.py
@@ -20,22 +20,6 @@ FILE_POSSIBILITIES = "abcdefgh"
 RANK_POSSIBILITIES = "87654321"
 
 
-def x_index_to_file(x: int) -> str:
-    return FILE_POSSIBILITIES[x]
-
-
-def y_index_to_rank(y: int) -> str:
-    return RANK_POSSIBILITIES[y]
-
-
-def rank_to_y_index(rank: str) -> int:
-    return 8 - int(rank)
-
-
-def file_to_x_index(file: str) -> int:
-    return string.ascii_lowercase.index(file)
-
-
 def get_algebraic_notation_mapping(
     board: Board, current_player: str, last_move: Move
 ) -> dict[str, Move]:
@@ -51,6 +35,22 @@ def get_algebraic_notation_mapping(
             **mapping,
         }
     return mapping
+
+
+def x_index_to_file(x: int) -> str:
+    return FILE_POSSIBILITIES[x]
+
+
+def y_index_to_rank(y: int) -> str:
+    return RANK_POSSIBILITIES[y]
+
+
+def rank_to_y_index(rank: str) -> int:
+    return 8 - int(rank)
+
+
+def file_to_x_index(file: str) -> int:
+    return string.ascii_lowercase.index(file)
 
 
 def _get_ambiguous_algebraic_notation_mapping(
