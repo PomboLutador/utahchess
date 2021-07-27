838f60d9348bce159bc8641f45aecde181eacfb7
Tizian Bluntschli <noemail>
2021-07-27 07:40:48 +0100

    Add more tests for algebraic mapping

--- tests/test_algebraic_notation.py
+++ tests/test_algebraic_notation.py
@@ -2,10 +2,13 @@ import pytest
 
 from utahchess.algebraic_notation import (
     file_to_x_index,
+    get_algebraic_notation_mapping,
     rank_to_y_index,
     x_index_to_file,
     y_index_to_rank,
 )
+from utahchess.board import Board
+from utahchess.move_validation import RegularMove, make_regular_move
 
 
 @pytest.mark.parametrize(
@@ -92,6 +95,238 @@ def test_file_to_x_index(file, expected_x_index):
     assert result == expected_x_index
 
 
-def test_implement_algebraic_notation_mapping_test():
-    # when and then
-    raise NotImplementedError("Implement tests for algebraic notation mapping!")
+@pytest.mark.parametrize(
+    ("board_string", "expected_legal_moves_in_algebraic_notation", "current_player"),
+    [
+        (
+            f"""bk-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-wk""",
+            ("Kb7", "Kb8", "Ka7"),
+            "black",
+        ),
+        (
+            f"""bk-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-wk""",
+            ("Kg2", "Kg1", "Kh2"),
+            "white",
+        ),
+        (
+            f"""bk-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-br-oo-oo-oo-wp-oo-wk""",
+            ("Kg2", "Kg1", "Kh2"),
+            "white",
+        ),
+        (
+            f"""bk-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            bb-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            wr-oo-oo-oo-oo-wp-oo-wk""",
+            ("Kb7", "Kb8", "Ka7"),
+            "black",
+        ),
+        (
+            f"""br-oo-oo-oo-bk-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-wk""",
+            (
+                "Kf8",
+                "Kf7",
+                "Kd7",
+                "Ke7",
+                "Kd8",
+                "O-O-O",
+                "Ra1",
+                "Ra2",
+                "Ra3",
+                "Ra4",
+                "Ra5",
+                "Ra6",
+                "Ra7",
+                "Rb8",
+                "Rc8",
+                "Rd8",
+            ),
+            "black",
+        ),
+        (
+            f"""bk-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            bb-oo-oo-oo-oo-oo-oo-bn
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-bn
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            wr-oo-oo-oo-oo-wp-oo-wk""",
+            (
+                "Kb7",
+                "Kb8",
+                "Ka7",
+                "N6f5",
+                "N4f5",
+                "Ng8",
+                "Nf7",
+                "Ng4",
+                "Nf3",
+                "Ng6",
+                "Ng2",
+            ),
+            "black",
+        ),
+        (
+            f"""bk-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            bb-oo-oo-oo-oo-bn-oo-bn
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            wr-oo-oo-oo-oo-wp-oo-wk""",
+            (
+                "Kb7",
+                "Kb8",
+                "Ka7",
+                "Nfg8",
+                "Nhg8",
+                "Nfg4",
+                "Nhg4",
+                "Nh5",
+                "Nf5",
+                "Nf7",
+                "Ne8",
+                "Ne4",
+                "Nd7",
+                "Nd5",
+                "Nh7",
+            ),
+            "black",
+        ),
+    ],
+)
+def test_get_algebraic_notation_mapping(
+    board_string, expected_legal_moves_in_algebraic_notation, current_player
+):
+    # given
+    board = Board(board_string=board_string)
+
+    # when
+    result = tuple(
+        get_algebraic_notation_mapping(
+            board=board, current_player=current_player, last_move=None
+        ).keys()
+    )
+
+    # then
+    assert set(expected_legal_moves_in_algebraic_notation) == set(result)
+
+
+@pytest.mark.parametrize(
+    (
+        "board_string",
+        "expected_legal_moves_in_algebraic_notation",
+        "current_player",
+        "pawn_move",
+    ),
+    [
+        (
+            f"""bk-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-bp-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-wp-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-wk""",
+            (
+                "Kb7",
+                "Kb8",
+                "Ka7",
+                "e3",
+                "xd3 e.p.",
+                "e2",
+            ),  # TODO: e2 is only possible with incorrent is_in_starting_position setting
+            "black",
+            (
+                (
+                    (3, 6),
+                    (3, 4),
+                ),
+            ),
+        ),
+        (
+            f"""bk-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-bp-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-wp-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-wk""",
+            (
+                "Kg2",
+                "Kg1",
+                "Kh2",
+                "d6",
+                "d7",
+                "xe6 e.p.",
+            ),  # TODO: d7 is only possible with incorrent is_in_starting_position setting
+            "white",
+            (
+                (
+                    (4, 1),
+                    (4, 3),
+                ),
+            ),
+        ),
+    ],
+)
+def test_get_algebraic_notation_mapping_with_last_move_for_en_passant(
+    board_string, expected_legal_moves_in_algebraic_notation, current_player, pawn_move
+):
+    # given
+    board = Board(board_string=board_string)
+    move_to_make = RegularMove(
+        piece_moves=pawn_move,
+        moving_pieces=(board[pawn_move[0][0]],),
+        is_capturing_move=False,
+        allows_en_passant=True,
+    )
+    board_after_move = make_regular_move(board=board, move=move_to_make)
+
+    # when
+    result = tuple(
+        get_algebraic_notation_mapping(
+            board=board_after_move,
+            current_player=current_player,
+            last_move=move_to_make,
+        ).keys()
+    )
+
+    # then
+    assert set(expected_legal_moves_in_algebraic_notation) == set(result)
