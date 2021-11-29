01e1432177fdf2f51ca188b44bd6e9e41c41515e
Tizian Bluntschli <tizian.bluntschli@gmail.com>
2021-11-29 22:23:07 +0000

    Update tests for move validation

--- tests/test_move_validation.py
+++ tests/test_move_validation.py
@@ -4,7 +4,8 @@ from utahchess.board import Board
 from utahchess.legal_moves import make_move
 from utahchess.move import Move
 from utahchess.move_candidates import get_king_move_candidates, get_pawn_move_candidates
-from utahchess.move_validation import is_checkmate
+from utahchess.move_validation import is_check, is_checkmate, is_valid_move
+from utahchess.regular_move import REGULAR_MOVE
 
 
 def test_is_checkmate_fools_mate():
@@ -71,119 +72,156 @@ def test_is_checkmate_another_scenario():
     assert not is_checkmate(board=board, current_player="black")
 
 
-# def test_validate_move_candidates_restricted_king():
-#     # when
-#     board_string = f"""oo-bn-bb-oo-oo-bb-oo-oo
-#             oo-oo-oo-oo-bn-oo-oo-br
-#             oo-oo-bp-oo-oo-oo-bn-oo
-#             oo-oo-bp-oo-bp-oo-oo-bp
-#             oo-oo-oo-wq-wn-bp-oo-wp
-#             wb-oo-oo-wp-oo-oo-oo-oo
-#             br-oo-oo-oo-oo-oo-oo-wk
-#             oo-oo-oo-oo-oo-wb-oo-wr"""
-#     board = Board(board_string=board_string)
-#     move_candidates = get_king_move_candidates(board=board, position=(7, 6))
-#     result = tuple(
-#         validate_move_candidates(board=board, move_candidates=move_candidates)
-#     )
+def test_is_valid_move_restricted_king():
+    # when
+    board_string = f"""oo-bn-bb-oo-oo-bb-oo-oo
+            oo-oo-oo-oo-bn-oo-oo-br
+            oo-oo-bp-oo-oo-oo-bn-oo
+            oo-oo-bp-oo-bp-oo-oo-bp
+            oo-oo-oo-wq-wn-bp-oo-wp
+            wb-oo-oo-wp-oo-oo-oo-oo
+            br-oo-oo-oo-oo-oo-oo-wk
+            oo-oo-oo-oo-oo-wb-oo-wr"""
+    board = Board(board_string=board_string)
 
-#     # then
-#     assert result == (
-#         Move(
-#             type=REGULAR_MOVE,
-#             piece_moves=(((7, 6), (6, 7)),),
-#             moving_pieces=(board[7, 6],),
-#             is_capturing_move=False,
-#             allows_en_passant=False,
-#         ),
-#     )
+    invalid_king_destinations = ((6, 6), (6, 5), (7, 5))
+    invalid_king_moves = tuple(
+        Move(
+            type=REGULAR_MOVE,
+            piece_moves=(((7, 6), invalid_king_destination),),
+            moving_pieces=(board[7, 6],),
+            is_capturing_move=False,
+            allows_en_passant=False,
+        )
+        for invalid_king_destination in invalid_king_destinations
+    )
+
+    valid_king_destinations = ((6, 7),)
+    valid_king_moves = tuple(
+        Move(
+            type=REGULAR_MOVE,
+            piece_moves=(((7, 6), valid_king_destination),),
+            moving_pieces=(board[7, 6],),
+            is_capturing_move=False,
+            allows_en_passant=False,
+        )
+        for valid_king_destination in valid_king_destinations
+    )
+
+    # then
+    assert all(not is_valid_move(board=board, move=move) for move in invalid_king_moves)
+    assert all(is_valid_move(board=board, move=move) for move in valid_king_moves)
+    assert (
+        len(
+            tuple(
+                get_king_move_candidates(
+                    board=board,
+                    position=(7, 6),
+                )
+            )
+        )
+        == 4
+    )
 
 
-# def test_validate_move_candidates_pawn_cant_move():
-#     # when
-#     board_string = f"""oo-bn-bb-oo-oo-bb-oo-oo
-#             bk-oo-oo-oo-oo-oo-oo-oo
-#             oo-oo-oo-oo-oo-oo-oo-oo
-#             oo-oo-oo-oo-oo-oo-oo-oo
-#             oo-oo-oo-oo-oo-oo-oo-oo
-#             oo-oo-oo-oo-oo-oo-oo-oo
-#             oo-oo-oo-oo-br-oo-wp-wk
-#             oo-oo-oo-oo-oo-oo-oo-oo"""
-#     board = Board(board_string=board_string)
-#     move_candidates = get_pawn_move_candidates(board=board, position=(6, 6))
-#     result = tuple(
-#         validate_move_candidates(board=board, move_candidates=move_candidates)
-#     )
+def test_is_valid_move_pawn_cant_move():
+    # when
+    board_string = f"""oo-bn-bb-oo-oo-bb-oo-oo
+            bk-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-br-oo-wp-wk
+            oo-oo-oo-oo-oo-oo-oo-oo"""
+    board = Board(board_string=board_string)
+    move_candidates = get_pawn_move_candidates(board=board, position=(6, 6))
 
-#     # then
-#     assert result == ()
+    # then
+    assert all(
+        tuple(
+            not is_valid_move(
+                board=board,
+                move=Move(
+                    type=REGULAR_MOVE,
+                    piece_moves=(move_candidate,),
+                    moving_pieces=(board[(6, 6)],),
+                    allows_en_passant=False,
+                    is_capturing_move=False,
+                ),
+            )
+            for move_candidate in move_candidates
+        )
+    )
 
 
-# @pytest.mark.parametrize(
-#     ("board", "from_move", "to_move", "expected"),
-#     [
-#         (
-#             Board(
-#                 board_string=f"""bk-oo-oo-oo-oo-oo-oo-oo
-#             oo-oo-oo-oo-oo-oo-oo-oo
-#             oo-oo-oo-oo-oo-oo-oo-oo
-#             oo-oo-oo-oo-oo-oo-oo-oo
-#             oo-oo-oo-oo-bp-oo-oo-oo
-#             oo-oo-oo-oo-oo-oo-oo-oo
-#             oo-oo-oo-wp-oo-oo-oo-oo
-#             oo-oo-oo-oo-oo-oo-oo-wk""",
-#             ),
-#             (7, 7),
-#             (7, 6),
-#             Board(
-#                 board_string=f"""bk-oo-oo-oo-oo-oo-oo-oo
-#             oo-oo-oo-oo-oo-oo-oo-oo
-#             oo-oo-oo-oo-oo-oo-oo-oo
-#             oo-oo-oo-oo-oo-oo-oo-oo
-#             oo-oo-oo-oo-bp-oo-oo-oo
-#             oo-oo-oo-oo-oo-oo-oo-oo
-#             oo-oo-oo-wp-oo-oo-oo-wk
-#             oo-oo-oo-oo-oo-oo-oo-oo""",
-#             ),
-#         ),
-#         (
-#             Board(
-#                 board_string=f"""bk-oo-oo-oo-oo-oo-oo-oo
-#             oo-oo-oo-oo-oo-oo-oo-oo
-#             oo-oo-oo-oo-oo-oo-oo-oo
-#             oo-oo-oo-oo-oo-oo-oo-oo
-#             oo-oo-oo-oo-bp-oo-oo-oo
-#             oo-oo-oo-oo-oo-oo-oo-oo
-#             oo-oo-oo-wp-oo-oo-oo-oo
-#             oo-oo-oo-oo-oo-oo-oo-wk""",
-#             ),
-#             (4, 4),
-#             (4, 5),
-#             Board(
-#                 board_string=f"""bk-oo-oo-oo-oo-oo-oo-oo
-#             oo-oo-oo-oo-oo-oo-oo-oo
-#             oo-oo-oo-oo-oo-oo-oo-oo
-#             oo-oo-oo-oo-oo-oo-oo-oo
-#             oo-oo-oo-oo-oo-oo-oo-oo
-#             oo-oo-oo-oo-bp-oo-oo-oo
-#             oo-oo-oo-wp-oo-oo-oo-oo
-#             oo-oo-oo-oo-oo-oo-oo-wk""",
-#             ),
-#         ),
-#     ],
-# )
-# def test_make_regular_move(board, from_move, to_move, expected):
-#     # given
-#     regular_move = Move(
-#         type=REGULAR_MOVE,
-#         piece_moves=((from_move, to_move),),
-#         moving_pieces=board[from_move],
-#         is_capturing_move=False,
-#         allows_en_passant=False,
-#     )
+@pytest.mark.parametrize(
+    ("board", "from_move", "to_move", "expected"),
+    [
+        (
+            Board(
+                board_string=f"""bk-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-bp-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-wp-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-wk""",
+            ),
+            (7, 7),
+            (7, 6),
+            Board(
+                board_string=f"""bk-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-bp-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-wp-oo-oo-oo-wk
+            oo-oo-oo-oo-oo-oo-oo-oo""",
+            ),
+        ),
+        (
+            Board(
+                board_string=f"""bk-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-bp-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-wp-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-wk""",
+            ),
+            (4, 4),
+            (4, 5),
+            Board(
+                board_string=f"""bk-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-oo
+            oo-oo-oo-oo-bp-oo-oo-oo
+            oo-oo-oo-wp-oo-oo-oo-oo
+            oo-oo-oo-oo-oo-oo-oo-wk""",
+            ),
+        ),
+    ],
+)
+def test_make_regular_move(board, from_move, to_move, expected):
+    # given
+    from utahchess.regular_move import REGULAR_MOVE
 
-#     # when
-#     result = make_move(board=board, move=regular_move)
+    regular_move = Move(
+        type=REGULAR_MOVE,
+        piece_moves=((from_move, to_move),),
+        moving_pieces=board[from_move],
+        is_capturing_move=False,
+        allows_en_passant=False,
+    )
 
-#     # then
-#     assert result == expected
+    # when
+    result = make_move(board=board, move=regular_move)
+
+    # then
+    assert result == expected
