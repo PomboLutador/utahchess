13aea3e8e71ab6395253ec2b583530944e7c3164
Tizian Bluntschli <tizian.bluntschli@gmail.com>
2021-11-29 22:36:05 +0000

    Run smoke test on more fixtures

--- tests/test_regular_move.py
+++ tests/test_regular_move.py
@@ -1,9 +1,28 @@
+import pytest
+
 from utahchess.board import Board
 from utahchess.regular_move import get_regular_moves
 
 
-def test_get_regular_moves():
+def test_get_regular_moves(
+    initial_board_with_only_rooks,
+    initial_board_with_only_bishops,
+    initial_board_with_only_queens,
+    initial_board_with_only_kings,
+):
     # when and then
     board = Board()
-    get_regular_moves(board=board, current_player="black")
-    get_regular_moves(board=board, current_player="white")
+    for current_player in ("white", "black"):
+        get_regular_moves(board=board, current_player=current_player)
+        get_regular_moves(
+            board=initial_board_with_only_rooks, current_player=current_player
+        )
+        get_regular_moves(
+            board=initial_board_with_only_bishops, current_player=current_player
+        )
+        get_regular_moves(
+            board=initial_board_with_only_queens, current_player=current_player
+        )
+        get_regular_moves(
+            board=initial_board_with_only_kings, current_player=current_player
+        )
