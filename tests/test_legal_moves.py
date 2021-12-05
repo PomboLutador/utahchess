7546397b9db58009fe398f45388dbffa886568e8
Tizian Bluntschli <tizian.bluntschli@gmail.com>
2021-12-05 12:25:16 +0000

    Remove comments and unused imports

--- tests/test_legal_moves.py
+++ tests/test_legal_moves.py
@@ -3,12 +3,6 @@ import pytest
 from utahchess.board import Board
 from utahchess.legal_moves import get_algebraic_notation_mapping, is_stalemate
 from utahchess.move import REGULAR_MOVE, Move, make_move
-from utahchess.utils import (
-    file_to_x_index,
-    rank_to_y_index,
-    x_index_to_file,
-    y_index_to_rank,
-)
 
 
 @pytest.mark.parametrize(
