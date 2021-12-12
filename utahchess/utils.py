5348c1f3540513f6c54264a26457ac125b8485dc
Tizian Bluntschli <tizian.bluntschli@gmail.com>
2021-12-12 19:16:21 +0000

    Improve board __repr__

--- utahchess/utils.py
+++ utahchess/utils.py
@@ -1,4 +1,7 @@
 import string
+from typing import Optional
+
+from utahchess.piece import Piece
 
 FILE_POSSIBILITIES = "abcdefgh"
 RANK_POSSIBILITIES = "87654321"
@@ -18,3 +21,39 @@ def rank_to_y_index(rank: str) -> int:
 
 def file_to_x_index(file: str) -> int:
     return string.ascii_lowercase.index(file)
+
+
+def get_unicode_character(piece: Optional[Piece]) -> str:
+    if piece is None:
+        return "|       | "
+    elif piece.piece_type == "Pawn":
+        if piece.color == "black":
+            return f"|   \u265F   | "
+        if piece.color == "white":
+            return f"|   \u2659   | "
+    elif piece.piece_type == "Knight":
+        if piece.color == "black":
+            return f"|   \u265E   | "
+        if piece.color == "white":
+            return f"|   \u2658   | "
+    elif piece.piece_type == "Rook":
+        if piece.color == "black":
+            return f"|   \u265C   | "
+        if piece.color == "white":
+            return f"|   \u2656   | "
+    elif piece.piece_type == "Bishop":
+        if piece.color == "black":
+            return f"|   \u265D   | "
+        if piece.color == "white":
+            return f"|   \u2657   | "
+    elif piece.piece_type == "Queen":
+        if piece.color == "black":
+            return f"|   \u265B   | "
+        if piece.color == "white":
+            return f"|   \u2655   | "
+    elif piece.piece_type == "King":
+        if piece.color == "black":
+            return f"|   \u265A   | "
+        if piece.color == "white":
+            return f"|   \u2654   | "
+    raise Exception(f"Could not determine piece {piece}")
