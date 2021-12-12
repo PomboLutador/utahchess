5d04ad9423649f8a86eb10706be8924239513399
PomboLutador <noemail>
2021-12-12 19:43:27 +0000

    Improve GUI main loop (#31)

--- gui/pygame/pygame_gui.py
+++ gui/pygame/pygame_gui.py
@@ -48,13 +48,12 @@ class PygameGUI:
                     self.running = False
 
                 elif event.type == pygame.MOUSEBUTTONDOWN:
-
                     self.visualize_current_game_state()
 
                     x_pixel, y_pixel = get_user_input()
                     x_index, y_index = get_tile_indices_from_user_input()
 
-                    # Handle button clicks
+                    # Handle button clicks on buttons
                     if self.new_game_button.collidepoint(x_pixel, y_pixel):
                         self.new_game()
                     if self.undo_move_button.collidepoint(x_pixel, y_pixel):
@@ -64,43 +63,14 @@ class PygameGUI:
                     if not is_in_bounds(position=(x_index, y_index)):
                         continue
 
-                    # If we click on an occupied tile, highlight legal moves
-                    if is_occupied(
-                        self.get_current_board(), position=(x_index, y_index)
-                    ):
-                        legal_destinations = self.game.get_legal_destinations_for_piece(
-                            position=(x_index, y_index)
-                        )
-                        highlight_legal_destinations(
-                            screen=self.screen,
-                            legal_destinations=legal_destinations,
-                            x=x_index,
-                            y=y_index,
-                        )
+                    # If click lands on an occupied tile, highlight legal destinations
+                    self._highlight_legal_destinations(x=x_index, y=y_index)
 
-                    # Not occupied or enemy color, try to make a move
-                    if not is_occupied(
-                        self.get_current_board(), (x_index, y_index)
-                    ) or is_edible(
-                        board=self.get_current_board(),
-                        position=(x_index, y_index),
-                        friendly_color=self.get_current_player(),
-                    ):
-                        if self.last_mouse_click_indices:
-                            # Make Move here
-                            potential_move = self.game.get_move_from_positions(
-                                from_position=self.last_mouse_click_indices,
-                                to_position=(x_index, y_index),
-                            )
-                            if potential_move:
-                                algebraic_move, _ = potential_move
-                            _ = self.game.make_move(
-                                move_in_algebraic_notation=algebraic_move
-                            )
-
-                            self.visualize_current_game_state()
+                    # If clicked tile not occupied or enemy color, try to make a move
+                    self._make_move(x=x_index, y=y_index)
 
                 elif event.type == pygame.MOUSEBUTTONUP:
+                    # On release of mouse button if the click was in bounds, it is saved
                     last_mouse_click_indices = convert_pixel_coordinates_to_indices(
                         pixel_coordinates=pygame.mouse.get_pos()
                     )
@@ -140,6 +110,36 @@ class PygameGUI:
 
     def get_opposite_player(self) -> str:
         return "black" if self.get_current_player() == "white" else "white"
+
+    def _highlight_legal_destinations(self, x, y) -> None:
+        if is_occupied(self.get_current_board(), position=(x, y)):
+            legal_destinations = self.game.get_legal_destinations_for_piece(
+                position=(x, y)
+            )
+            highlight_legal_destinations(
+                screen=self.screen,
+                legal_destinations=legal_destinations,
+                x=x,
+                y=y,
+            )
+
+    def _make_move(self, x, y) -> None:
+        if not is_occupied(self.get_current_board(), (x, y)) or is_edible(
+            board=self.get_current_board(),
+            position=(x, y),
+            friendly_color=self.get_current_player(),
+        ):
+            if self.last_mouse_click_indices:
+                # Make Move here
+                potential_move = self.game.get_move_from_positions(
+                    from_position=self.last_mouse_click_indices,
+                    to_position=(x, y),
+                )
+                if potential_move:
+                    algebraic_move, _ = potential_move
+                _ = self.game.make_move(move_in_algebraic_notation=algebraic_move)
+
+                self.visualize_current_game_state()
 
 
 if __name__ == "__main__":
